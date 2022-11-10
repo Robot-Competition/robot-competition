dd3d2f256004ff6fa598f74d6796da407126956e
Lewis Trundle <lewis.trundle.1510@gmail.com>
2022-11-10 11:11:47 +0000

    #21 Final track centered in page

--- components/trackBox.js
+++ components/trackBox.js
@@ -3,8 +3,12 @@ class TrackBox extends HTMLElement {
       var id = this.attributes.id.value;
       var image = this.attributes.image.value;
       var description = this.attributes.description.value;
+      var clss = "";
+      if (this.attributes.center) {
+        clss = "custom-container-center"
+      }
       this.innerHTML = `
-      <div class="img-box b-${id}">
+      <div class="img-box b-${id} ${clss}">
         <a href="tracks.html#${id}">
           <img src=${image} alt="">
           <div class="btn-box">
